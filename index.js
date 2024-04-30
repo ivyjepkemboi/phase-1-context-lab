33763c7eb8f51ed745630c4cf2a2895eb968bb99
Ivy <ivyjkemboi@gmail.com>
2024-04-30 17:39:04 +0300

    context_lab

--- index.js
+++ index.js
@@ -1,23 +1,77 @@
-/* Your Code Here */
+// Helper function to calculate hours worked between two timestamps
+function hoursWorked(timeIn, timeOut) {
+    const timeInHour = parseInt(timeIn.split(' ')[1]);
+    const timeOutHour = parseInt(timeOut.split(' ')[1]);
+    return timeOutHour - timeInHour;
+}
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+// Function to create an employee record
+function createEmployeeRecord(arr) {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+        createTimeInEvent: function(timeStamp) {
+            const [date, hour] = timeStamp.split(' ');
+            this.timeInEvents.push({ type: "TimeIn", hour: parseInt(hour), date: date });
+            return this;
+        },
+        createTimeOutEvent: function(timeStamp) {
+            const [date, hour] = timeStamp.split(' ');
+            this.timeOutEvents.push({ type: "TimeOut", hour: parseInt(hour), date: date });
+            return this;
+        },
+        hoursWorkedOnDate: function(date) {
+            const timeIn = this.timeInEvents.find(event => event.date === date).hour;
+            const timeOut = this.timeOutEvents.find(event => event.date === date).hour;
+            return hoursWorked(timeIn, timeOut);
+        },
+        wagesEarnedOnDate: function(date) {
+            const hoursWorked = this.hoursWorkedOnDate(date);
+            return hoursWorked * this.payPerHour;
+        }
+    };
+}
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+// Function to create employee records from array of arrays
+function createEmployeeRecords(arr) {
+    return arr.map(createEmployeeRecord);
+}
 
-const allWagesFor = function () {
+// Function to find employee record by first name
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(record => record.firstName === firstName);
+}
+
+// Function to calculate all wages for an employee
+function allWagesFor() {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
-    })
+    });
 
     const payable = eligibleDates.reduce(function (memo, d) {
         return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+    }.bind(this), 0);
 
-    return payable
+    return payable;
 }
 
+// Function to calculate total payroll for all employees
+function calculatePayroll(arr) {
+    return arr.reduce((totalPayroll, employee) => totalPayroll + allWagesFor.call(employee), 0);
+}
+
+module.exports = {
+    createEmployeeRecord,
+    createEmployeeRecords,
+    createTimeInEvent,
+    createTimeOutEvent,
+    hoursWorkedOnDate,
+    wagesEarnedOnDate,
+    allWagesFor,
+    findEmployeeByFirstName,
+    calculatePayroll
+};
